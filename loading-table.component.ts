c31c9e75f2de6f25cd415f3f1586c8815ac36ad6
anderk222 <anderk222@gmail.com>
2023-08-31 22:41:47 -0500

    put loading table component ro only a file

--- loading-table.component.ts
+++ loading-table.component.ts
@@ -2,7 +2,6 @@ import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-loading-table',
-  templateUrl: './loading-table.component.html',
   template: `
   <style>
   .bg-loading{
@@ -12,17 +11,14 @@ import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
   }
   </style>
   <div class="w-full flex flex-col justify-center text-zinc-700 items-center gap-2 h-80" >
-
     <figure [style]="{ 'background-image' : 'url(' + photos[status] +')' }" class="bg-loading w-44 h-44" >
     </figure>
     <p *ngIf="status =='LOAD'" class="text-center" >Cargando...</p>
     <p *ngIf="status =='NOTHING'" class="text-center" >No hay elementos que mostrar</p>
     <p *ngIf="status =='ERROR'" class="text-center text-red-500"> Ha ocurrido un error</p>
-
-</div>
-
-`,
-  styleUrls: ['./loading-table.component.css']
+  </div>
+  `,
+  styleUrls: []
 })
 export class LoadingTableComponent implements OnChanges {
 
